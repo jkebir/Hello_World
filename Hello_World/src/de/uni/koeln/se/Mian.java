d2867e2ea877ba3815b754d601f8722e1bc530fd
jkebir <92728456+jkebir@users.noreply.github.com>
2021-10-18 17:32:50 +0200

    Update Mian.java
    neu hier

--- Hello_World/src/de/uni/koeln/se/Mian.java
+++ Hello_World/src/de/uni/koeln/se/Mian.java
@@ -4,6 +4,7 @@ public class Mian {
 	public static void main(String[]args) {
 		System.out.println("Hello World");
 		System.out.println("Hello Jonas");
+		System.out.println("Ich bin der neue");
 
 		
 	}
